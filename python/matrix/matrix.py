28c82770f0b13f674ee055b7f1f772c97f0910d8
CHIPK <chipk215@msn.com>
2020-09-11 11:28:59 -0600

    Updating matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,26 +1,14 @@
 class Matrix:
     def __init__(self, matrix_string):
-        self.rows = matrix_string.split("\n")
-
-    def check_index(self, index):
-        max_rows = len(self.rows)
-        if index < 1:
-            raise ValueError(f"index {index} must be greater than 0.")
-        if index > max_rows:
-            raise ValueError(f"index {index} must be less than  than {max_rows}.")
+        strings = matrix_string.split("\n")
+        self.rows = [[int(item) for item in row] for row in [string_value.split() for string_value in strings]]
 
     def row(self, index):
-        self.check_index(index)
-        return self.rows(index - 1)
+        return self.rows[index - 1]
 
     def column(self, index):
-        if index < 1:
-            raise ValueError(f"index {index} must be greater than 0.")
         col = []
         for row in self.rows:
-            if index > len(row):
-                raise ValueError(f"col index {index} invlaid for row {row}.")
-
             col.append(row[index - 1])
 
         return col
